8fa0fd3686c3dc72ec068829a0439197aa211fd9
Eric Butler <eric@codebutler.com>
2013-08-08 15:57:33 -0700

    [library] Check current SDK version instead of target SDK verion.
    Fixes #23, crash on 2.3 devices.

--- library/src/main/java/org/lucasr/twowayview/TwoWayView.java
+++ library/src/main/java/org/lucasr/twowayview/TwoWayView.java
@@ -68,6 +68,8 @@ import android.widget.Checkable;
 import android.widget.ListAdapter;
 import android.widget.Scroller;
 
+import static android.os.Build.VERSION_CODES.HONEYCOMB;
+
 /*
  * Implementation Notes:
  *
@@ -4818,8 +4820,7 @@ public class TwoWayView extends AdapterView<ListAdapter> implements
         if (mChoiceMode != ChoiceMode.NONE && mCheckStates != null) {
             if (child instanceof Checkable) {
                 ((Checkable) child).setChecked(mCheckStates.get(position));
-            } else if (getContext().getApplicationInfo().targetSdkVersion
-                    >= Build.VERSION_CODES.HONEYCOMB) {
+            } else if (Build.VERSION.SDK_INT >= HONEYCOMB) {
                 child.setActivated(mCheckStates.get(position));
             }
         }
@@ -5442,16 +5443,13 @@ public class TwoWayView extends AdapterView<ListAdapter> implements
         final int firstPos = mFirstPosition;
         final int count = getChildCount();
 
-        final boolean useActivated = getContext().getApplicationInfo().targetSdkVersion
-                >= Build.VERSION_CODES.HONEYCOMB;
-
         for (int i = 0; i < count; i++) {
             final View child = getChildAt(i);
             final int position = firstPos + i;
 
             if (child instanceof Checkable) {
                 ((Checkable) child).setChecked(mCheckStates.get(position));
-            } else if (useActivated) {
+            } else if (Build.VERSION.SDK_INT >= HONEYCOMB) {
                 child.setActivated(mCheckStates.get(position));
             }
         }
