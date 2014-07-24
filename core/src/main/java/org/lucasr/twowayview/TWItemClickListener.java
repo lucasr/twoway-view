73bd1a8c88a1ae8efd4305b7a264e6f7e2afc313
Lucas Rocha <lucasr@lucasr.org>
2014-07-24 22:31:16 +0100

    [core] Use GestureDetector to implement TWItemClickListener

--- core/src/main/java/org/lucasr/twowayview/TWItemClickListener.java
+++ core/src/main/java/org/lucasr/twowayview/TWItemClickListener.java
@@ -1,15 +1,15 @@
 package org.lucasr.twowayview;
 
-import android.graphics.Rect;
+import android.content.Context;
 import android.os.Build;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.LayoutManager;
 import android.support.v7.widget.RecyclerView.OnItemTouchListener;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewConfiguration;
 
 public class TWItemClickListener implements OnItemTouchListener {
     private static final String LOGTAG = "TWItemClickListener";
@@ -50,38 +50,18 @@ public class TWItemClickListener implements OnItemTouchListener {
         boolean onItemLongClick(RecyclerView parent, View view, int position, long id);
     }
 
-    private static final int INVALID_POINTER = -1;
-    private static final int INVALID_POSITION = -1;
-
-    private static final int TOUCH_MODE_REST = -1;
-    private static final int TOUCH_MODE_DOWN = 0;
-    private static final int TOUCH_MODE_TAP = 1;
-    private static final int TOUCH_MODE_DONE_WAITING = 2;
 
     private final RecyclerView mHostView;
+    private final GestureDetector mGestureDetector;
 
     private OnItemClickListener mItemClickListener;
     private OnItemLongClickListener mItemLongClickListener;
 
-    private int mTouchMode = TOUCH_MODE_REST;
-    private final int mTouchSlop;
-    private int mMotionPosition;
-    private float mLastTouchPos;
-    private float mTouchRemainderPos;
-    private int mActivePointerId;
-
-    private final Rect mTempRect = new Rect();
-
-    private CheckForTap mPendingCheckForTap;
-    private CheckForLongPress mPendingCheckForLongPress;
-    private PerformClick mPerformClick;
-    private Runnable mTouchModeReset;
-
     private TWItemClickListener(RecyclerView hostView) {
         mHostView = hostView;
 
-        final ViewConfiguration vc = ViewConfiguration.get(mHostView.getContext());
-        mTouchSlop = vc.getScaledTouchSlop();
+        final Context context = mHostView.getContext();
+        mGestureDetector = new ItemClickGestureDetector(context, new ItemClickGestureListener());
     }
 
     private boolean isAttachedToWindow() {
@@ -92,82 +72,8 @@ public class TWItemClickListener implements OnItemTouchListener {
         }
     }
 
-    private boolean isVertical() {
-        final LayoutManager layout = mHostView.getLayoutManager();
-        if (layout == null) {
-            return true;
-        }
-
-        return layout.canScrollVertically();
-    }
-
     private boolean hasAdapter() {
         return (mHostView.getAdapter() != null);
-    }
-
-    private View getViewAtPosition(int position) {
-        return mHostView.getChildAt(position - getFirstVisiblePosition());
-    }
-
-    private int getFirstVisiblePosition() {
-        final View child = mHostView.getChildAt(0);
-        if (child == null) {
-            return 0;
-        }
-
-        final RecyclerView.LayoutParams lp =
-                (RecyclerView.LayoutParams) child.getLayoutParams();
-        return lp.getViewPosition();
-    }
-
-    private int pointToPosition(int x, int y) {
-        final int childCount = mHostView.getChildCount();
-        for (int i = childCount - 1; i >= 0; i--) {
-            final View child = mHostView.getChildAt(i);
-
-            if (child.getVisibility() == View.VISIBLE) {
-                child.getHitRect(mTempRect);
-
-                if (mTempRect.contains(x, y)) {
-                    return getFirstVisiblePosition() + i;
-                }
-            }
-        }
-
-        return INVALID_POSITION;
-    }
-
-    private void triggerCheckForTap() {
-        if (mPendingCheckForTap == null) {
-            mPendingCheckForTap = new CheckForTap();
-        }
-
-        mHostView.postDelayed(mPendingCheckForTap, ViewConfiguration.getTapTimeout());
-    }
-
-    private void cancelCheckForTap() {
-        if (mPendingCheckForTap == null) {
-            return;
-        }
-
-        mHostView.removeCallbacks(mPendingCheckForTap);
-    }
-
-    private void triggerCheckForLongPress() {
-        if (mPendingCheckForLongPress == null) {
-            mPendingCheckForLongPress = new CheckForLongPress();
-        }
-
-        mHostView.postDelayed(mPendingCheckForLongPress,
-                ViewConfiguration.getLongPressTimeout());
-    }
-
-    private void cancelCheckForLongPress() {
-        if (mPendingCheckForLongPress == null) {
-            return;
-        }
-
-        mHostView.removeCallbacks(mPendingCheckForLongPress);
     }
 
     @Override
@@ -176,131 +82,7 @@ public class TWItemClickListener implements OnItemTouchListener {
             return false;
         }
 
-        final boolean isVertical = isVertical();
-
-        final int action = event.getAction() & MotionEventCompat.ACTION_MASK;
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                final float x = event.getX();
-                final float y = event.getY();
-
-                mLastTouchPos = (isVertical ? y : x);
-                mActivePointerId = MotionEventCompat.getPointerId(event, 0);
-                mTouchRemainderPos = 0;
-
-                mMotionPosition = pointToPosition((int) x, (int) y);
-
-                if (mMotionPosition >= 0) {
-                    mTouchMode = TOUCH_MODE_DOWN;
-                    triggerCheckForTap();
-                }
-                break;
-
-            case MotionEvent.ACTION_MOVE: {
-                if (mTouchMode != TOUCH_MODE_DOWN) {
-                    return false;
-                }
-
-                final int index = MotionEventCompat.findPointerIndex(event, mActivePointerId);
-                if (index < 0) {
-                    Log.e(LOGTAG, "onInterceptTouchEvent could not find pointer with id " +
-                            mActivePointerId + " - did RecyclerView receive an inconsistent " +
-                            "event stream?");
-                    return false;
-                }
-
-                final float pos;
-                if (isVertical) {
-                    pos = MotionEventCompat.getY(event, index);
-                } else {
-                    pos = MotionEventCompat.getX(event, index);
-                }
-
-                final float diff = pos - mLastTouchPos + mTouchRemainderPos;
-                final int delta = (int) diff;
-                mTouchRemainderPos = diff - delta;
-
-                if (Math.abs(delta) > mTouchSlop) {
-                    cancelCheckForTap();
-                    cancelCheckForLongPress();
-
-                    mTouchMode = TOUCH_MODE_REST;
-                }
-
-                break;
-            }
-
-            case MotionEvent.ACTION_CANCEL:
-                cancelCheckForTap();
-                mTouchMode = TOUCH_MODE_REST;
-
-                final View motionView = getViewAtPosition(mMotionPosition);
-                if (motionView != null) {
-                    motionView.setPressed(false);
-                }
-
-                break;
-
-
-            case MotionEvent.ACTION_UP:
-                if (mTouchMode != TOUCH_MODE_DOWN &&
-                    mTouchMode != TOUCH_MODE_TAP &&
-                    mTouchMode != TOUCH_MODE_DONE_WAITING) {
-                    return false;
-                }
-
-                final View child = getViewAtPosition(mMotionPosition);
-                if (child != null && !child.hasFocusable()) {
-                    if (mTouchMode != TOUCH_MODE_DOWN) {
-                        child.setPressed(false);
-                    }
-
-                    if (mPerformClick == null) {
-                        mPerformClick = new PerformClick();
-                    }
-
-                    final PerformClick performClick = mPerformClick;
-                    performClick.clickMotionPosition = mMotionPosition;
-
-                    if (mTouchMode == TOUCH_MODE_DOWN || mTouchMode == TOUCH_MODE_TAP) {
-                        if (mTouchMode == TOUCH_MODE_DOWN) {
-                            cancelCheckForTap();
-                        } else {
-                            cancelCheckForLongPress();
-                        }
-
-                        mTouchMode = TOUCH_MODE_TAP;
-
-                        child.setPressed(true);
-
-                        if (mTouchModeReset != null) {
-                            mHostView.removeCallbacks(mTouchModeReset);
-                        }
-
-                        mTouchModeReset = new Runnable() {
-                            @Override
-                            public void run() {
-                                mTouchMode = TOUCH_MODE_REST;
-
-                                child.setPressed(false);
-
-                                performClick.run();
-                                mTouchModeReset = null;
-                            }
-                        };
-
-                        mHostView.postDelayed(mTouchModeReset,
-                                ViewConfiguration.getPressedStateDuration());
-                    } else {
-                        performClick.run();
-                    }
-                }
-
-                mTouchMode = TOUCH_MODE_REST;
-                mActivePointerId = INVALID_POINTER;
-                break;
-        }
-
+        mGestureDetector.onTouchEvent(event);
         return false;
     }
 
@@ -340,66 +122,100 @@ public class TWItemClickListener implements OnItemTouchListener {
         return listener;
     }
 
-    private class PerformClick implements Runnable {
-        int clickMotionPosition;
+    private class ItemClickGestureDetector extends GestureDetector {
+        private final ItemClickGestureListener mGestureListener;
+
+        public ItemClickGestureDetector(Context context, ItemClickGestureListener listener) {
+            super(context, listener);
+            mGestureListener = listener;
+        }
 
         @Override
-        public void run() {
-            final RecyclerView.Adapter adapter = mHostView.getAdapter();
-            final int itemCount = (adapter != null ? adapter.getItemCount() : 0);
-            final int pos = clickMotionPosition;
+        public boolean onTouchEvent(MotionEvent event) {
+            final boolean handled = super.onTouchEvent(event);
 
-            if (itemCount > 0 && pos != INVALID_POSITION && pos < adapter.getItemCount()) {
-                final View child = getViewAtPosition(pos);
-                if (child != null && mItemClickListener != null) {
-                    mItemClickListener.onItemClick(mHostView, child, pos, adapter.getItemId(pos));
-                }
+            final int action = event.getAction() & MotionEventCompat.ACTION_MASK;
+            if (action == MotionEvent.ACTION_UP) {
+                mGestureListener.dispatchSingleTapUpIfNeeded(event);
             }
+
+            return handled;
         }
     }
 
-    private final class CheckForTap implements Runnable {
-        @Override
-        public void run() {
-            if (mTouchMode != TOUCH_MODE_DOWN) {
-                return;
-            }
+    private class ItemClickGestureListener extends SimpleOnGestureListener {
+        private View mTargetChild;
 
-            mTouchMode = TOUCH_MODE_TAP;
-
-            final View child = getViewAtPosition(mMotionPosition);
-            if (child != null && !child.hasFocusable()) {
-                child.setPressed(true);
-
-                if (mHostView.isLongClickable()) {
-                    triggerCheckForLongPress();
-                } else {
-                    mTouchMode = TOUCH_MODE_DONE_WAITING;
-                }
+        public void dispatchSingleTapUpIfNeeded(MotionEvent event) {
+            // When the long press hook is called but the long press listener
+            // returns false, the target child will be left around to be
+            // handled later. In this case, we should still treat the gesture
+            // as potential item click.
+            if (mTargetChild != null) {
+                onSingleTapUp(event);
             }
         }
-    }
 
-    private class CheckForLongPress implements Runnable {
         @Override
-        public void run() {
-            final RecyclerView.Adapter adapter = mHostView.getAdapter();
-            final int pos = mMotionPosition;
+        public boolean onDown(MotionEvent event) {
+            final int x = (int) event.getX();
+            final int y = (int) event.getY();
 
-            final View child = getViewAtPosition(pos);
-            if (child != null) {
-                final long longPressId = adapter.getItemId(mMotionPosition);
+            mTargetChild = mHostView.findChildViewUnder(x, y);
+            return (mTargetChild != null);
+        }
 
-                boolean handled = false;
+        @Override
+        public void onShowPress(MotionEvent event) {
+            if (mTargetChild != null) {
+                mTargetChild.setPressed(true);
+            }
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent event) {
+            if (mTargetChild != null) {
+                mTargetChild.setPressed(false);
+
+                if (mItemClickListener != null) {
+                    final int position = mHostView.getChildPosition(mTargetChild);
+                    final long id = mHostView.getAdapter().getItemId(position);
+                    mItemClickListener.onItemClick(mHostView, mTargetChild, position, id);
+
+                    return true;
+                }
+
+                mTargetChild = null;
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent event, MotionEvent event2, float v, float v2) {
+            if (mTargetChild != null) {
+                mTargetChild.setPressed(false);
+                mTargetChild = null;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent event) {
+            if (mTargetChild != null) {
                 if (mItemLongClickListener != null) {
-                    handled = mItemLongClickListener.onItemLongClick(mHostView, child, pos, longPressId);
-                }
+                    final int position = mHostView.getChildPosition(mTargetChild);
+                    final long id = mHostView.getAdapter().getItemId(position);
+                    final boolean handled = mItemLongClickListener.onItemLongClick(mHostView,
+                            mTargetChild, position, id);
 
-                if (handled) {
-                    mTouchMode = TOUCH_MODE_REST;
-                    child.setPressed(false);
-                } else {
-                    mTouchMode = TOUCH_MODE_DONE_WAITING;
+                    if (handled) {
+                        mTargetChild.setPressed(false);
+                        mTargetChild = null;
+                    }
                 }
             }
         }
