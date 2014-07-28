1284656a93cb8a7c355e58901567cbd6559f28ac
Lucas Rocha <lucasr@lucasr.org>
2014-07-28 14:09:46 +0100

    [core] Add layoutManager XML attribute to TWView

--- core/src/main/java/org/lucasr/twowayview/TWView.java
+++ core/src/main/java/org/lucasr/twowayview/TWView.java
@@ -17,14 +17,25 @@
 package org.lucasr.twowayview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 import org.lucasr.twowayview.TWLayoutManager;
 import org.lucasr.twowayview.TWLayoutManager.Orientation;
+import org.w3c.dom.Attr;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class TWView extends RecyclerView {
     private static final String LOGTAG = "TWView";
+
+    private static final Class<?>[] sConstructorSignature = new Class[] {
+            Context.class, AttributeSet.class};
+
+    final Object[] sConstructorArgs = new Object[2];
 
     public TWView(Context context) {
         this(context, null);
@@ -36,6 +47,38 @@ public class TWView extends RecyclerView {
 
     public TWView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+        final TypedArray a =
+                context.obtainStyledAttributes(attrs, R.styleable.TWView, defStyle, 0);
+
+        final String name = a.getString(R.styleable.TWView_layoutManager);
+        if (!TextUtils.isEmpty(name)) {
+            loadLayoutManagerFromName(context, attrs, name);
+        }
+
+        a.recycle();
+    }
+
+    private void loadLayoutManagerFromName(Context context, AttributeSet attrs, String name) {
+        try {
+            if (name.startsWith(".")) {
+                final String packageName = context.getPackageName();
+                name = packageName + name;
+            }
+
+            Class<? extends TWLayoutManager> clazz =
+                    context.getClassLoader().loadClass(name).asSubclass(TWLayoutManager.class);
+
+            Constructor<? extends TWLayoutManager> constructor =
+                    clazz.getConstructor(sConstructorSignature);
+
+            sConstructorArgs[0] = context;
+            sConstructorArgs[1] = attrs;
+
+            setLayoutManager(constructor.newInstance(sConstructorArgs));
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not load TWLayoutManager from class: " + name, e);
+        }
     }
 
     @Override
